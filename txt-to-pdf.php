012b7e5662658374523fa3ad0cca132b1d6f343f
Dejie1 <dejieang168@gmail.com>
2023-12-08 21:43:04 +0800

    ✨ feat: complete files to zip conversion code

--- txt-to-pdf.php
+++ txt-to-pdf.php
@@ -22,14 +22,19 @@
     </form>
     <?php
     if (isset($_FILES['txtFile']['name'])) {
+        // Define the paths for the input file, output directory, and output file
+        $inputPath = "/home/dejie/pdf-to-txt/input/";
+        $outputPath = "/home/dejie/pdf-to-txt/output/";
+        // Create a new zip archive
+        $zip = new ZipArchive();
+        $zipFile = $outputPath . '/converted_files.zip';
+        if ($zip->open($zipFile, ZipArchive::CREATE | ZipArchive::OVERWRITE) !== TRUE) {
+            exit("Cannot open <$zipFile>\n");
+        }
         // Loop over each submitted file
         foreach ($_FILES['txtFile']['tmp_name'] as $i => $tmp_name) {
             $name = $_FILES['txtFile']['name'][$i];
             // echo "Uploaded file name: " . $name . "<br>";
-
-            // Define the paths for the input file, output directory, and output file
-            $inputPath = "/home/dejie/pdf-to-txt/input/";
-            $outputPath = "/home/dejie/pdf-to-txt/output/";
 
             $inputFile = $inputPath . $name;
             $outputFile = $outputPath . preg_replace('/\.[^.]+$/', '.pdf', $name);
@@ -46,38 +51,23 @@
 
             // Check if the conversion was successful
             if (file_exists($outputFile)) {
-                echo "File exists";
-
-                // Get the filename with extension
-                // $fileName = preg_replace('/\.[^.]+$/', '.txt', basename($name));
-
-                // Set the headers to force the browser to download the file
-                // header("Content-Type: application/txt");
-                // header("Content-Disposition: attachment; filename=" . $fileName);
-
-                // Read the file content into a variable
-                // readfile($outputFile);
-                $url = str_replace('/home/dejie/pdf-to-txt/', '', $outputFile);
-                echo "<a href=\"" . $url . "\" download>Download your PDF:  " . basename($outputFile) . "</a><br>";
-                // $fileContent = file_get_contents($outputFile);
-
-                // Send the file content to the browser
-                // echo $fileContent;
-
-                // Register a shutdown function to delete the file
-                // register_shutdown_function(function () use ($outputFile, $inputFile) {
-                //   unlink($outputFile);
-                //   // Unlink the input file
-                //   unlink($inputFile);
-                // });
+                // Add the file to the zip
+                $zip->addFile($outputFile, basename($outputFile));
             } else {
                 // If the conversion failed, display an error message and delete the input file
-                echo "Error converting Txt!";
+                echo "Error converting TXT!";
                 if (file_exists($inputFile)) {
                     unlink($inputFile);
                 }
             }
         }
+
+        // Close the zip file
+        $zip->close();
+
+        // Provide a download link for the zip file
+        $url = str_replace('/home/dejie/pdf-to-txt/', '', $zipFile);
+        echo "<a href=\"" . $url . "\" download>Download " . basename($zipFile) . "</a><br>";
     } else {
         echo "Please select a TXT file to upload.";
     }
